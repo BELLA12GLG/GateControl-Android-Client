4fa0d44da5235ab720c09be6ddef9a03b3ef545d
BELLA12GLG <71425111+BELLA12GLG@users.noreply.github.com>
2026-06-13 04:08:38 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 31
         targetSdk = 35
         versionCode = 10616
-        versionName = "1.6.19"
+        versionName = "1.6.20"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
